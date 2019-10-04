e42b6bd51523f0490c8710d280e46efcae128aea
William Lukas <iggyboy456@gmail.com>
2019-10-04 13:51:23 -0500

    basic inquirer

--- greatBay.js
+++ greatBay.js
@@ -1,5 +1,7 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
+require("dotenv").config();
+var chalk = require("chalk");
 
 //inquirer stuff
 //
@@ -10,32 +12,84 @@ let inquirer = require("inquirer");
 //
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "",
-    database: ""
-  });
+  host: "GreatBay.db.4473603.faf.hostedresource.net",
 
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    //do stuff
-  });
+  // Your port; if not 3306
+  port: 3306,
 
-  function bid(){
-      //bidding stuff
-  }
+  // Your username
+  user: "GreatBay",
 
-  function post(){
-      //posting stuff
-  }
+  // Your password
+  password: process.env.DB_PASSWORD,
+  database: "GreatBay"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  // these post and bid, both are hardcoded, this should change.
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What'ya sellin?",
+      name: "productName"
+    },
+    {
+      type: "input",
+      message: "Where'ya sellin?",
+      name: "category"
+    },
+    {
+      type: "input",
+      message: "How much?",
+      name: "price"
+    }
+  ]).then(function (data){
+    post(data.productName, data.category, data.price);
+  })
+
+  // bid("Cocaine", 10);
+  // post("Cocaine", "Drugs", "5");
+});
+
+function bid(item, bid) {
+  //bidding stuff
+  console.log("Posting your bid\n");
+  var query = connection.query(
+    "UPDATE AuctionItems SET ? WHERE ?",
+    [
+      {
+        CurrentBid: bid,
+      },
+      {
+        Product: item
+      }
+    ],
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " listing updated!\n");
+    });
+}
+
+function post(item, category, price) {
+  //posting stuff
+  console.log("Posting your bid\n");
+  var query = connection.query(
+    "INSERT INTO AuctionItems SET ?",
+    {
+      Product: item,
+      StartingBid: price,
+      Category: category,
+      CurrentBid: price,
+      BuyItNow: price
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " listing posted!\n");
+    });
+}
 
 
 
